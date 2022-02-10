835b30d4ba7963f7bf82831465651db85f53b54c
shaliniagrahari20 <99171137+shaliniagrahari20@users.noreply.github.com>
2022-02-10 01:54:43 -0800

    Add files via upload

---
+++ pro.py
@@ -0,0 +1,5 @@
+team1=["jake","Allan","Nick","Dave"]
+team2=["David","john","Chric","Alex","Nick"]
+for aplayer in team1:
+    if aplayer in team2:
+            print("%s also plays for team2."%(aplayer))
